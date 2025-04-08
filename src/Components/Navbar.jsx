c9ebff8cdd8bae9d74fcb670a9d81dba3f3873fa
Tanishk Agrawal065 <tanishkagrawalprsnl@Tanishks-MacBook-Pro.local>
2025-04-08 09:44:56 +0530

    login page changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-// eslint-disable-next-line no-unused-vars
+
 import React, { useState } from 'react'
 import { assets } from '../assets/assets'
 import { NavLink, useNavigate } from 'react-router-dom'
